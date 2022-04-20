6118b9ee62ceb5dff7a18d7f20ecf988da4a16de
nikitagru <grunix123@gmail.com>
2022-04-20 23:40:46 +0500

    Initial commit

---
+++ src/test/java/nikitagru/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package nikitagru.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
